de070a90111e9ac6e371acae58a2c55ed99e54b7
Sivhong Deab <sivhong.deab@wingmoney.com>
2022-09-02 13:45:36 +0700

    add rounded button

---
+++ lib/button_collection.dart
@@ -0,0 +1,40 @@
+library rounded_button;
+
+import 'package:flutter/material.dart';
+
+class RoundedButton extends StatelessWidget {
+  final Color? backgroundColor;
+  final Widget child;
+  final VoidCallback onTap;
+  final Border? borderStyle;
+  final BorderRadius? borderRadius;
+  final EdgeInsets padding;
+  const RoundedButton({
+    required this.onTap,
+    required this.child,
+    this.borderStyle,
+    this.borderRadius,
+    this.padding = const EdgeInsets.all(10.0),
+    this.backgroundColor = Colors.blueAccent,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: InkWell(
+        borderRadius: borderRadius,
+        onTap: onTap,
+        child: Ink(
+          padding: padding,
+          decoration: BoxDecoration(
+            color: backgroundColor,
+            border: borderStyle,
+            borderRadius: borderRadius ?? BorderRadius.circular(15.0),
+          ),
+          child: child,
+        ),
+      ),
+    );
+  }
+}
